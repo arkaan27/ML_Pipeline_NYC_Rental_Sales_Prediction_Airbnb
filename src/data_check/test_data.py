8222a6f6b4f4abc2cd327b29809de6ebe92cb0ed
arkaan27 <arkaankanunga@gmail.com>
2022-02-06 14:02:30 +0000

    Completed the data_check component of the pipeline, Added to the main.py file

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -59,7 +59,16 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+def test_row_count(data):
+    """
+    Tests the row count of the dataset
+    """
+    assert 15000 < data.shape[0] < 1000000
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+
+def test_price_range(data,min_price,max_price):
+    """
+    Checks the price range in the dataset is between minimum and maximum price range
+    """
+    assert data['price'].between(min_price,max_price)
+
